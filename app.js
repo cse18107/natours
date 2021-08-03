f939791754af98412d66f44cdaeb5ed235d7cc5d
cse18107 <cse18107@cemk.ac.in>
2021-08-03 16:50:26 +0530

    Handling get method

--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 const experss = require('express');
-
+const fs = require('fs');
 const app = experss();
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello from the server side!', app: 'Natours' });
-});
-
-app.post('/', (req, res) => {
-  res.send('You can post to this endpoing...');
+// app.get('/', (req, res) => {
 //   res
 //     .status(200)
-//     .json({ message: 'You can post to this endpoint..', app: 'Natours' });
+//     .json({ message: 'Hello from the server side!', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can post to this endpoing...');
+// //   res
+// //     .status(200)
+// //     .json({ message: 'You can post to this endpoint..', app: 'Natours' });
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+    res.status(200).json({
+        status:'success',
+        results:tours.length,
+        data:{
+            tours:tours
+        }
+    });
 });
 
 const port = 3000;
