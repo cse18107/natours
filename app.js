7583bb525ffda6a91588b380d23fde74e0e14535
cse18107 <cse18107@cemk.ac.in>
2021-08-03 16:04:20 +0530

    doing some practice with HTTP methods get and post

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const experss=require('express');
+const experss = require('express');
 
-const app=experss();
+const app = experss();
 
-app.get('/',(res,req)=>{
-    
+app.get('/', (req, res) => {
+  res
+    .status(200)
+    .json({ message: 'Hello from the server side!', app: 'Natours' });
 });
 
+app.post('/', (req, res) => {
+  res.send('You can post to this endpoing...');
+//   res
+//     .status(200)
+//     .json({ message: 'You can post to this endpoint..', app: 'Natours' });
+});
 
-const port =3000;
-app.listen(port , () =>{
-    console.log(`App running on port ${port}...`);
+const port = 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
