5544c3e209b05f5743bf7c8c89961c1a4259fddd
cse18107 <cse18107@cemk.ac.in>
2021-08-05 22:22:13 +0530

    server work 1

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,23 +1,20 @@
 const fs = require('fs');
-const express=require('express');
-const tourController=require('./../controllers/tourController');
+const express = require('express');
+const tourController = require('../controllers/tourController');
+
 const router = express.Router();
 
-router.param('id',tourController.checkID);
+// router.param('id',tourController.checkID);
 
-// Create a checkBody middleware
-// Check if body contains the name and price property
-// If not,send back 404 (bad request)
-//Add it to the post handler stack
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
 
-
-
-
-
-router.route('/').get(tourController.getAllTours).post(tourController.checkBody,tourController.createTour);
-
-
-
-router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
 
 module.exports = router;
